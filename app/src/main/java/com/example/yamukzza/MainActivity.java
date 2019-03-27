874a1dae41ada82b6588a6cfbe871b06295ff4cc
Limcc <ghostar98@gmail.com>
2019-03-27 22:34:00 +0900

    1

---
+++ app/src/main/java/com/example/yamukzza/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.yamukzza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Toolbar myToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        //setSupportActionBar(myToolbar);
+    }
+}
